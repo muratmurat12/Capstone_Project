c68d3048c72b7bd0253d243d36f0ab4cd23c40d6
muratmurat12 <109490113+muratmurat12@users.noreply.github.com>
2023-02-15 13:02:21 -0500

    Update MainActivity.java
    MainActivity

--- ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.shoppinglist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+     private ActivityMainBinding binding;
+    public ArrayList<ClipData.Item> itemArrayList;
+    ItemAdaptor itemAdaptor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
