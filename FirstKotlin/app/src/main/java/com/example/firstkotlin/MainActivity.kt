f63c3e3704e78093da1bda78250d03290e1d28df
muratmurat12 <109490113+muratmurat12@users.noreply.github.com>
2023-03-07 17:25:55 -0500

    Update MainActivity.kt

--- FirstKotlin/app/src/main/java/com/example/firstkotlin/MainActivity.kt
+++ FirstKotlin/app/src/main/java/com/example/firstkotlin/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.firstkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+        private lateinit var editTextName: EditText
+        private lateinit var textViewName: TextView
+        private lateinit var buttonSave: Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editTextName = findViewById(R.id.editTextName)
+        textViewName = findViewById(R.id.textViewName)
+        buttonSave =findViewById(R.id.buttonSave)
+        buttonSave.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val name = editTextName.text.toString().trim()
+                textViewName.text ="Hello, $name"
+            }
+        })
     }
 }
