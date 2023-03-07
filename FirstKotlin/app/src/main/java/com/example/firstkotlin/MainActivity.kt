4fa5dd3f4c979c7715db63c0ba9757f99c9dacda
muratmurat12 <109490113+muratmurat12@users.noreply.github.com>
2023-03-07 17:36:44 -0500

    MainActivity

--- FirstKotlin/app/src/main/java/com/example/firstkotlin/MainActivity.kt
+++ FirstKotlin/app/src/main/java/com/example/firstkotlin/MainActivity.kt
@@ -6,19 +6,17 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
-        private lateinit var editTextName: EditText
-        private lateinit var textViewName: TextView
-        private lateinit var buttonSave: Button
+
+
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        editTextName = findViewById(R.id.editTextName)
-        textViewName = findViewById(R.id.textViewName)
-        buttonSave =findViewById(R.id.buttonSave)
+
         buttonSave.setOnClickListener {
             val name = editTextName.text.toString().trim()
             textViewName.text = "Hello, $name"
