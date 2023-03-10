82c81fe5b6030f64747053b423a0810e5570d76c
muratmurat12 <109490113+muratmurat12@users.noreply.github.com>
2023-03-10 17:38:30 -0500

    Update CoinInfo.kt

--- CryptoApp/app/src/main/java/com/example/cryptoapp/pojo/CoinInfo.kt
+++ CryptoApp/app/src/main/java/com/example/cryptoapp/pojo/CoinInfo.kt
@@ -1,4 +1,10 @@
 package com.example.cryptoapp.pojo
 
-class CoinInfo {
-}
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class CoinInfo (
+    @SerializedName("Name")
+    @Expose
+    val name: String? = null
+)
