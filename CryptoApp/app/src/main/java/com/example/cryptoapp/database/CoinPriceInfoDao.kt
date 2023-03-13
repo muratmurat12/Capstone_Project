ed2fb1bc48ad16b5ed94f2144907bd5826e2f711
muratmurat12 <109490113+muratmurat12@users.noreply.github.com>
2023-03-13 17:25:30 -0400

    Update CoinPriceInfoDao.kt

--- CryptoApp/app/src/main/java/com/example/cryptoapp/database/CoinPriceInfoDao.kt
+++ CryptoApp/app/src/main/java/com/example/cryptoapp/database/CoinPriceInfoDao.kt
@@ -1,4 +1,9 @@
 package com.example.cryptoapp.database
 
+import androidx.room.Dao
+
+@Dao
 interface CoinPriceInfoDao {
+    @Query("SELECT * FROM full_price_list ORDER BY lastUpdate DESC")
+    fun getPriceList(): LiveData<List<CoinPriceInfo>>
 }
