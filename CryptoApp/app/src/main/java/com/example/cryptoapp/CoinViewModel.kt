418d36716040452cd8377ff5f1c55c35bea29132
muratmurat12 <109490113+muratmurat12@users.noreply.github.com>
2023-03-13 18:42:36 -0400

    Update CoinViewModel.kt

--- CryptoApp/app/src/main/java/com/example/cryptoapp/CoinViewModel.kt
+++ CryptoApp/app/src/main/java/com/example/cryptoapp/CoinViewModel.kt
@@ -1,4 +1,18 @@
 package com.example.cryptoapp
 
-class CoinViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.example.cryptoapp.database.AppDatabase
+import io.reactivex.disposables.CompositeDisposable
+
+class CoinViewModel(application: Application):AndroidViewModel(application) {
+    private val db = AppDatabase.getInstance(application)
+    private val compositeDisposable = CompositeDisposable()
+
+    val priceList = db.coinPriceInfoDao().getPriceList()
+
+    fun loadData(){
+
+    }
+
 }
