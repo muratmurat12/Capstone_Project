a99b7586dd84dd18ea1d3bf8afed7d2c98279de2
muratmurat12 <109490113+muratmurat12@users.noreply.github.com>
2023-03-14 21:41:49 -0400

    ApiFactory

--- CryptoApp/app/src/main/java/com/example/cryptoapp/pojo/CoinPriceInfo.kt
+++ CryptoApp/app/src/main/java/com/example/cryptoapp/pojo/CoinPriceInfo.kt
@@ -3,6 +3,8 @@ package com.example.cryptoapp.pojo
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.cryptoapp.api.ApiFactory.BASE_IMAGE_URL
+import com.example.cryptoapp.utils.convertTimestampToTime
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
@@ -126,4 +128,12 @@ data class CoinPriceInfo(
     @SerializedName("IMAGEURL")
     @Expose
     val imageUrl: String?
-)
+){
+    fun getFormattedTime(): String {
+        return convertTimestampToTime(lastUpdate)
+    }
+
+    fun getFullImageUrl(): String {
+        return BASE_IMAGE_URL + imageUrl
+    }
+}
