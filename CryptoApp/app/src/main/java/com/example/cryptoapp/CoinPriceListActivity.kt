8b2d3b06468e0ef7671e31d5fa4b31213f9059d3
muratmurat12 <109490113+muratmurat12@users.noreply.github.com>
2023-03-14 23:21:42 -0400

    Update CoinPriceListActivity.kt

--- CryptoApp/app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
+++ CryptoApp/app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
@@ -2,49 +2,34 @@ package com.example.cryptoapp
 
 import CoinViewModel
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-
-//import kotlinx.android.synthetic.main.activity_coin_detail.*
+import com.example.cryptoapp.adapters.CoinInfoAdapter
+import com.example.cryptoapp.pojo.CoinPriceInfo
+import kotlinx.android.synthetic.main.activity_coin_price_list.*
 
 class CoinPriceListActivity : AppCompatActivity() {
 
-     private lateinit var viewModel: CoinViewModel
-
-//    private val compositeDisposable = CompositeDisposable()
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//        val disposable = ApiFactory.apiService.getTopCoinsInfo()
-//            .subscribeOn(Schedulers.io())
-//            .observeOn(AndroidSchedulers.mainThread())
-//            .subscribe({
-//                   Log.d("TEST_OF_LOADING_DATA", "Success: $it")
-//            },{
-//                   Log.d("TEST_OF_LOADING_DATA", "Failure: ${it.message}")
-//            })
-//        compositeDisposable.add(disposable)
-//
-//    }
-//
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        compositeDisposable.dispose()
-//    }
+    private lateinit var viewModel: CoinViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coin_price_list)
+        val adapter = CoinInfoAdapter(this)
+        adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
+            override fun onCoinClick(coinPriceInfo: CoinPriceInfo) {
+//                val intent = CoinDetailActivity.newIntent(
+//                    this@CoinPriceListActivity,
+//                    coinPriceInfo.fromSymbol
+//                )
+                startActivity(intent)
+            }
+        }
+        rvCoinPriceList.adapter = adapter
         viewModel = ViewModelProviders.of(this)[CoinViewModel::class.java]
-        viewModel.loadData()
-        viewModel.priceList.observe(this,Observer{
-            Log.d("TEST_OF_LOADING_DATA", "Success in activity: $it")
+        viewModel.priceList.observe(this, Observer {
+            adapter.coinInfoList = it
         })
-
-
-
     }
 }
