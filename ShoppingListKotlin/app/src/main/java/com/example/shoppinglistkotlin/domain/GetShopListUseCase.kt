b94150dc99abf3af6f58ca9c2ac8f099de590060
muratmurat12 <109490113+muratmurat12@users.noreply.github.com>
2023-03-16 18:49:07 -0400

    Update GetShopListUseCase.kt

--- ShoppingListKotlin/app/src/main/java/com/example/shoppinglistkotlin/domain/GetShopListUseCase.kt
+++ ShoppingListKotlin/app/src/main/java/com/example/shoppinglistkotlin/domain/GetShopListUseCase.kt
@@ -1,8 +1,9 @@
 package com.example.shoppinglistkotlin.domain
 
-class GetShopListUseCase {
+class GetShopListUseCase (private val shopListRepository: ShopListRepository) {
 
     fun getShopList(): List<ShopItem>{
-        TODO()
+        
+        return shopListRepository.getShopList()
     }
 }
