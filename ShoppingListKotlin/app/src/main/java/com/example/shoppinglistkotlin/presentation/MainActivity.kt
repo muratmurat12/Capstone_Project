bd67cc5e95803fbe0cf651e8449126ba51988220
muratmurat12 <109490113+muratmurat12@users.noreply.github.com>
2023-03-16 18:03:45 -0400

    main

--- ShoppingListKotlin/app/src/main/java/com/example/shoppinglistkotlin/presentation/MainActivity.kt
+++ ShoppingListKotlin/app/src/main/java/com/example/shoppinglistkotlin/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.shoppinglistkotlin
+package com.example.shoppinglistkotlin.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shoppinglistkotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
