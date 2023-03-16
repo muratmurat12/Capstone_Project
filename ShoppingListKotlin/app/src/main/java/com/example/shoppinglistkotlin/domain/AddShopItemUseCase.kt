4229d062c9260263856c7385a8067a9f4c8e41a1
muratmurat12 <109490113+muratmurat12@users.noreply.github.com>
2023-03-16 18:48:30 -0400

    Update AddShopItemUseCase.kt

--- ShoppingListKotlin/app/src/main/java/com/example/shoppinglistkotlin/domain/AddShopItemUseCase.kt
+++ ShoppingListKotlin/app/src/main/java/com/example/shoppinglistkotlin/domain/AddShopItemUseCase.kt
@@ -1,8 +1,8 @@
 package com.example.shoppinglistkotlin.domain
 
-class AddShopItemUseCase {
+class AddShopItemUseCase (private val shopListRepository: ShopListRepository){
 
       fun addShopItem(shopItem: ShopItem){
-        TODO()
+        shopListRepository.addShopItem(shopItem)
     }
 }
