4e1429509f9cebad68981ef7da17c5d2f99a4f9c
muratmurat12 <109490113+muratmurat12@users.noreply.github.com>
2023-03-16 18:48:40 -0400

    Update DeleteShopItemUseCase.kt

--- ShoppingListKotlin/app/src/main/java/com/example/shoppinglistkotlin/domain/DeleteShopItemUseCase.kt
+++ ShoppingListKotlin/app/src/main/java/com/example/shoppinglistkotlin/domain/DeleteShopItemUseCase.kt
@@ -1,8 +1,8 @@
 package com.example.shoppinglistkotlin.domain
 
-class DeleteShopItemUseCase {
+class DeleteShopItemUseCase (private val shopListRepository: ShopListRepository){
 
-      fun deleteShopItem(ShopItem: ShopItem){
-        TODO()
+      fun deleteShopItem(shopItem: ShopItem){
+        shopListRepository.deleteShopItem(shopItem)
     }
 }
