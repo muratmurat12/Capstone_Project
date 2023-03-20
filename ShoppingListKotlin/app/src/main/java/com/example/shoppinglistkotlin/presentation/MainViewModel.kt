12967939045aa1693084080d9b46efe752c982a9
muratmurat12 <109490113+muratmurat12@users.noreply.github.com>
2023-03-20 17:05:23 -0400

    Update MainViewModel.kt

--- ShoppingListKotlin/app/src/main/java/com/example/shoppinglistkotlin/presentation/MainViewModel.kt
+++ ShoppingListKotlin/app/src/main/java/com/example/shoppinglistkotlin/presentation/MainViewModel.kt
@@ -1,4 +1,31 @@
 package com.example.shoppinglistkotlin.presentation
 
-class MainViewModel {
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglistkotlin.data.ShopListRepositoryImpl
+import com.example.shoppinglistkotlin.domain.DeleteShopItemUseCase
+import com.example.shoppinglistkotlin.domain.EditShopItemUseCase
+import com.example.shoppinglistkotlin.domain.GetShopListUseCase
+import com.example.shoppinglistkotlin.domain.ShopItem
+
+class MainViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = getShopListUseCase.getShopList()
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+    }
 }
