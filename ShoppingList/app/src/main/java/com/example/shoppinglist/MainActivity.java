543dca2e5481dc146984807108458a7fb31fb401
muratmurat12 <109490113+muratmurat12@users.noreply.github.com>
2023-03-28 15:58:08 -0400

    Update MainActivity.java

--- ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -1,8 +1,26 @@
 package com.example.shoppinglist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.shoppinglist.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
         getData();
 
     }
-        public void getData(){
+
+    public void getData(){
         try {
             SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("Items",MODE_PRIVATE,null);
             Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM items",null);
@@ -34,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
             int quantityIx = cursor.getColumnIndex("quantity");
             int isFavoriteIx = cursor.getColumnIndex("is_favorite");
 
-              while (cursor.moveToNext()){
+            while (cursor.moveToNext()){
                 int id = cursor.getInt(idIx);
                 String name = cursor.getString(nameIx);
                 String price = cursor.getString(priceIx);
@@ -43,4 +62,95 @@ public class MainActivity extends AppCompatActivity {
                 Item item = new Item(id,name,price,quantity,isFavorite);
                 itemArrayList.add(item);
             }
+
+            itemAdaptor.notifyDataSetChanged();
+            cursor.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.shopping_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.add_item){
+            Intent intent = new Intent(this,AddItemActivity.class);
+            startActivity(intent);
+        }
+        else if (item.getItemId() == R.id.favoritesPage){
+            Intent intent = new Intent(this,Favorites.class);
+            startActivity(intent);
+        }
+        else if (item.getItemId() == R.id.shoppingList){
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void calculateTotal(View view){
+        double totalCost = 0;
+        try {
+            SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("Items",MODE_PRIVATE,null);
+            Cursor cursor = sqLiteDatabase.rawQuery("SELECT Price,quantity FROM items",null);
+
+            int priceIx = cursor.getColumnIndex("Price");
+            int quantityIx = cursor.getColumnIndex("quantity");
+
+            while (cursor.moveToNext()){
+
+                String price = cursor.getString(priceIx);
+                String quantity = cursor.getString(quantityIx);
+
+                double priceDouble = Double.parseDouble(price);
+                double quantityDouble = Double.parseDouble(quantity);
+
+
+                totalCost += (priceDouble*quantityDouble);
+
+                binding.totalCost.setText("Total cost: " + totalCost);
+            }
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT) {
+
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            try {
+                SQLiteDatabase sqLiteDatabase = getApplicationContext().openOrCreateDatabase("Items",MODE_PRIVATE,null);
+                int position  = itemArrayList.get(viewHolder.getAdapterPosition()).id;
+                String deleteStatement = "DELETE FROM Items WHERE id = ?";
+                SQLiteStatement sqLiteStatement = sqLiteDatabase.compileStatement(deleteStatement);
+                sqLiteStatement.bindLong(1,position);
+                sqLiteStatement.execute();
+                itemAdaptor.notifyItemRemoved(position);
+                itemArrayList.remove(viewHolder.getAdapterPosition());
+                Toast.makeText(MainActivity.this,"Deleted Successfully",Toast.LENGTH_SHORT).show();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    };
 }
