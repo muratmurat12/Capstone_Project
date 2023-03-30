2e02683385528147415d9616f29f1417194d0abe
muratmurat12 <109490113+muratmurat12@users.noreply.github.com>
2023-03-30 16:56:01 -0400

    Update ShoppingMapsActivity.java

--- ShoppingList/app/src/main/java/com/example/shoppinglist/ShoppingMapsActivity.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/ShoppingMapsActivity.java
@@ -1,8 +1,15 @@
 package com.example.shoppinglist;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,10 +19,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.shoppinglist.databinding.ActivityShoppingMapsBinding;
 
+import java.util.List;
+
 public class ShoppingMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityShoppingMapsBinding binding;
+
+    List <Address> listGeoCode;
+    private static final int LOCATION_PERMISSION_CODE =101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +36,28 @@ public class ShoppingMapsActivity extends FragmentActivity implements OnMapReady
         binding = ActivityShoppingMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        if (isLocationPG()){
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+
+            try{
+                listGeoCode = new Geocoder(this).getFromLocationName("Maxi chemin Chambly",1);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+            double logitude = listGeoCode.get(0).getLongitude();
+            double latitude = listGeoCode.get(0).getLatitude();
+            Log.i("GOOGLE_MAP_TAG","Addres has Longitude"+
+                    "::: "+ String.valueOf(logitude) +"Latitude"+ String.valueOf(latitude));
+        }
+       else {
+           requestLocationPermission();
+        }
+
     }
 
     /**
@@ -47,5 +77,25 @@ public class ShoppingMapsActivity extends FragmentActivity implements OnMapReady
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        }
+
+    }
+    private boolean isLocationPG(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    private void requestLocationPermission(){
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                LOCATION_PERMISSION_CODE);
     }
 }
