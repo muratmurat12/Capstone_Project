85af36f103c537d48c8279973d2140f1b2eee947
muratmurat12 <109490113+muratmurat12@users.noreply.github.com>
2023-04-07 17:36:06 -0400

    Update MainActivity.java

--- Stripe_Android/app/src/main/java/com/example/stripe_android/MainActivity.java
+++ Stripe_Android/app/src/main/java/com/example/stripe_android/MainActivity.java
@@ -3,6 +3,18 @@ package com.example.stripe_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.pay_now);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    public void fetchApi(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://projects.vishnusivadas.com/testing/write.php";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            protected Map<String, String> getParams(){
+                Map<String, String> paramV = new HashMap<>();
+                paramV.put("param", "abc");
+                return paramV;
+            }
+        };
+        queue.add(stringRequest);
     }
 }
