cd808df775ba5dbd7921d161295c9bb04a83dcbf
muratmurat12 <109490113+muratmurat12@users.noreply.github.com>
2023-04-13 17:22:31 -0400

    Create ShopItemViewHolder.kt

---
+++ ShoppingListKotlin/app/src/main/java/com/example/shoppinglistkotlin/presentation/ShopItemViewHolder.kt
@@ -0,0 +1,13 @@
+package com.example.shoppinglistkotlin.presentation
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shoppinglistkotlin.R
+
+
+
+class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+    val tvName = view.findViewById<TextView>(R.id.tv_name)
+    val tvCount = view.findViewById<TextView>(R.id.tv_count)
+}
